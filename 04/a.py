3cc92b4310a1c40932fa10d8450f13bdbcc0356a
Simon Junod <sj@simonjunod.ch>
2022-12-04 11:30:00 +0100

    Day 4 done

---
+++ 04/a.py
@@ -0,0 +1,15 @@
+with open("input", "r") as f:
+    lines = f.read().splitlines()
+
+number = 0
+for line in lines:
+    ra, rb = line.split(",")
+    fa, ta = map(int, ra.split("-"))
+    fb, tb = map(int, rb.split("-"))
+    ra = set(range(fa, ta + 1))
+    rb = set(range(fb, tb + 1))
+    ri = ra.intersection(rb)
+    if ri == ra or ri == rb:
+        number += 1
+
+print(number)
